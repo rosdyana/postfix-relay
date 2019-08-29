1ae02cbecaf1286063d7f25c6e04153e15584be9
rosdyana <rosdyana.kusuma@gmail.com>
2019-08-29 16:34:21 +0800

    [-] rework test email script

--- code/test_email.py
+++ code/test_email.py
@@ -1,20 +1,37 @@
 import smtplib
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
-from email.header import Header
+from email.mime.base import MIMEBase
+from email.utils import COMMASPACE, formatdate
+from email.encoders import encode_base64
+import os
 
-smtp = smtplib.SMTP()
-# smtp.connect('mail.rosyid.info', 25)
-smtp.connect('localhost')
 
-msgRoot = MIMEMultipart("alternative")
-msgRoot['Subject'] = Header("Subject subject", "utf-8")
-msgRoot['From'] = "admin@mail.rosyid.info"
-msgRoot['To'] = "rosdyana.kusuma@gmail.com"
-text = MIMEText(open('template.txt', 'r').read(), "plain", "utf-8")
-msgRoot.attach(text)
-html = MIMEText(open('template.html', 'r').read(), "html", "utf-8")
-msgRoot.attach(html)
-smtp.sendmail("admin@mail.rosyid.info", "rosdyana.kusuma@gmail.com", msgRoot.as_string())
+def sendMail(to, fro, subject, text, files=[], server="localhost"):
+    assert type(to) == list
+    assert type(files) == list
+
+    msg = MIMEMultipart()
+    msg['From'] = fro
+    msg['To'] = COMMASPACE.join(to)
+    msg['Date'] = formatdate(localtime=True)
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(text))
+
+    for file in files:
+        part = MIMEBase('application', "octet-stream")
+        part.set_payload(open(file, "rb").read())
+        encode_base64(part)
+        part.add_header('Content-Disposition', 'attachment; filename="%s"'
+                        % os.path.basename(file))
+        msg.attach(part)
+
+    smtp = smtplib.SMTP(server)
+    smtp.sendmail(fro, to, msg.as_string())
+    smtp.close()
+
+
+# Example:
+sendMail(['Rosdyana Kusuma <rosdyana.kusuma@gmail.com>'], 'Admin <admin@mail.rosyid.info>',
+         'Hello Python!', 'Heya buddy! Say hello to Python! :)', ['template.txt'])
